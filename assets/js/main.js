c87611270242e899ba0ec3f1c4d6c11a360bf140
diego-cavenati <diegocavenati@gmail.com>
2022-11-08 15:31:55 +0100

    Add email list after they are load

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,22 @@
 Attraverso l'apposita API di Boolean https://flynn.boolean.careers/exercises/api/random/mail generare 10 indirizzi email e stamparli in pagina all'interno di una lista. 
 */
 
+const { createApp } = Vue;
+
+createApp ({
+    data(){
+        return{
+            emails : [],
+        }
+    },
+    mounted() {
+        for (let index = 0; index < 10; index++) {
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then(response => {
+                this.emails.push(response.data.response);
+                // console.log(response.data.response);
+            })
+        }
+    }
+}).mount('#app')   
